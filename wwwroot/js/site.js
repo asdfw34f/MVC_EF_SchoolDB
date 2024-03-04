d42b2cf00cc64039b13d215c82e35c7e2ee4aa5e
asdfw34f <daniil2002.d11@gmail.com>
2024-03-04 17:33:34 +0300

    temp | to finish create test panel

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -34,5 +34,6 @@ $(document).ready(function () {
 $('.input-file input[type=file]').on('change', function () {
     let file = this.files[0];
     $(this).closest('.input-file').find('.input-file-text').html(file.name);
+    
 });
 
